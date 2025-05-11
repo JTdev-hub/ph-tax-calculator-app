2940988c37a61d6fef6f1eb171c33bf63713eccb
tubleranel <jrrtuble@gmail.com>
2025-05-11 13:14:25 +0800

    add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["ph-tax.PNG"],
+      manifest: {
+        name: "PH Tax Calculator",
+        short_name: "PHTaxApp",
+        description: "PH Tax Calculator App",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "ph-tax.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "ph-tax.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
