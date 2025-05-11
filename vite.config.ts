192c771c077ff5bf02fc465ebb18ff9821147826
tubleranel <jrrtuble@gmail.com>
2025-05-11 13:39:29 +0800

    update ph-tax.PNG and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
       manifest: {
         name: "PH Tax Calculator",
         short_name: "PHTaxApp",
